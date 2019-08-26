a6e7f92ffe92fb7fe287cb4a184d6f1bdcf30e4a
Paavo Poutiainen <paavopoutiainen90@gmail.com>
2019-08-26 20:46:36 +0300

    step5

--- kurssitiedot/src/index.js
+++ kurssitiedot/src/index.js
@@ -26,8 +26,7 @@ const Part = (props) => {
 }
 
 const Total = (props) => {
-    const reducer = (accum, current) => accum + current;
- 
+  
     const exerciseNumbers = props.parts.map(a => a.exercises);
     console.log(exerciseNumbers);
 
@@ -41,28 +40,29 @@ const Total = (props) => {
 
 
 const App = () => {
-    const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-  
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+          {
+            name: 'Fundamentals of React',
+            exercises: 10
+          },
+          {
+            name: 'Using props to pass data',
+            exercises: 7
+          },
+          {
+            name: 'State of a component',
+            exercises: 14
+          }
+        ]
+      }
 
   return (
     <div>
-      <Header course = {course}/>
-      <Content parts = {parts} />
-      <Total parts = {parts} />
+      <Header course = {course.name}/>
+      <Content parts = {course.parts} />
+      <Total parts = {course.parts} />
     </div>
   )
 }
