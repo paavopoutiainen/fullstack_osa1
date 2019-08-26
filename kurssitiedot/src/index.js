471afadd9b89f5a8676b6a5d3783efdc4c91a95a
Paavo Poutiainen <paavopoutiainen90@gmail.com>
2019-08-26 09:25:44 +0300

    stepp1

--- kurssitiedot/src/index.js
+++ kurssitiedot/src/index.js
@@ -1,6 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const Header = (props) => {
+    return(
+        <h1>{props.course}</h1>
+    )
+}
+
+const Content = (props) => {
+    return(
+        <div>
+            <p>{props.part1} {props.exercises1}</p>
+            <p>{props.part2} {props.exercises2}</p>
+            <p>{props.part3}{props.exercises3}</p>
+        </div>
+    )
+}
+
+const Total = (props) => {
+    return(
+        <div>
+            <p>Number of exercises {props.total}</p>
+        </div>
+    )
+}
 
 const App = () => {
   const course = 'Half Stack application development'
@@ -13,17 +36,11 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course = {course}/>
+      <Content part1 = {part1} part2 = {part2} part3 = {part3} 
+      exercises1 ={exercises1} exercises2 = {exercises2} 
+      exercises3 = {exercises3} />
+      <Total total = {exercises1 + exercises2 + exercises3} />
     </div>
   )
 }
